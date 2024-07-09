1b53488a3884cdc14e9493240c0bfb9607d4df24
Babbymed <babbyzbourgmed@gmail.com>
2024-07-09 18:02:01 +0100

    short form.R

--- main_analyse.R
+++ main_analyse.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(here)
 
-tv_hours_tab <- gss_cat %>% 
+tv_hours_tabil <- gss_cat %>% 
   filter(age <30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
-write_csv(tv_hours_tab, here("tv_hours_by_marital.csv"))
+write_csv(tv_hours_tabil, here("tv_hours_by_marital.csv"))
